42079f90273ddcba8060322a50a905f2edfe4afe
autosunpakorn <autorobot41@gmail.com>
2024-05-18 05:49:35 +0900

    add sentiment type

--- run_inference.py
+++ run_inference.py
@@ -1,10 +1,12 @@
 import os
 import json
 from typing import List
+
 # from LM.sentiment_build import tokenizer, model
 from LM.sentiment_pipeline import pipe as sentiment_model
- 
-tokenizer_kwargs = {'padding': True, 'truncation': True, 'max_length': 512}
+
+tokenizer_kwargs = {"padding": True, "truncation": True, "max_length": 512}
+
 
 def get_split_max_length(c: str, ret: List = []):
     """
@@ -14,7 +16,7 @@ def get_split_max_length(c: str, ret: List = []):
         ret.append(c)
         return ret
     else:
-        last_space_idx_bf_512 = c[:512].rfind('<_>')
+        last_space_idx_bf_512 = c[:512].rfind("<_>")
         content_first = c[:last_space_idx_bf_512]
         content_second = c[last_space_idx_bf_512:]
         ret.append(content_first)
@@ -25,16 +27,16 @@ def get_sentiment(year: int, month: int, day: int):
     """
     Get the sentiment from the content for a given year, month, and day.
     """
-    open_path = os.path.join('text_jsonl', str(year), str(month).zfill(2))
-    save_path = os.path.join('sentiment_jsonl', str(year), str(month).zfill(2))
+    open_path = os.path.join("text_jsonl", str(year), str(month).zfill(2))
+    save_path = os.path.join("sentiment_jsonl", str(year), str(month).zfill(2))
     if not os.path.exists(save_path):
         os.makedirs(save_path, exist_ok=True)
- 
-    with open(save_path + '/' + str(day).zfill(2) + '.jsonl', 'w', encoding='utf8') as outfile:
-        with open(open_path + '/' + str(day).zfill(2) + '.jsonl', 'r', encoding='utf8') as infile:
+
+    with open(save_path + "/" + str(day).zfill(2) + ".jsonl", "w", encoding="utf8") as outfile:
+        with open(open_path + "/" + str(day).zfill(2) + ".jsonl", "r", encoding="utf8") as infile:
             for line in infile:
                 data = json.loads(line)
-                content = data['content']
+                content = data["content"]
                 new_content = []
                 for c in content:
                     if len(c) <= 512:
@@ -43,25 +45,29 @@ def get_sentiment(year: int, month: int, day: int):
                         new_content.extend(get_split_max_length(c))
                 sentiment = sentiment_model(new_content, **tokenizer_kwargs)
                 for idx, s in enumerate(sentiment):
-                    s['len_str'] = len(new_content[idx])
+                    s["len_str"] = len(new_content[idx])
+                avg_score = sum([s["score"] for s in sentiment]) / len(sentiment)
                 ret = {
-                    'date': f'{year}-{month}-{day}',
-                    'topic': data['topic'],
-                    'content': data['content'],
-                    'sentiment': sentiment,
+                    "date": f"{year}-{month}-{day}",
+                    "topic": data["topic"],
+                    "content": data["content"],
+                    "sentiment": sentiment,
+                    "avg_score": avg_score,
+                    "sentiment_type": "positive" if avg_score > 0.5 else "negative",
                 }
-                jout = json.dumps(ret, ensure_ascii=False) + '\n'
+                jout = json.dumps(ret, ensure_ascii=False) + "\n"
                 outfile.write(jout)
-                
+
 
 def get_sentiment_all_dates():
     """
     Get the sentiment from the content for all dates.
     """
-    for year in os.listdir('text_jsonl'):
-        for month in os.listdir(os.path.join('text_jsonl', year)):
-            for day in os.listdir(os.path.join('text_jsonl', year, month)):
-                get_sentiment(int(year), int(month), int(day))
+    for year in os.listdir("text_jsonl"):
+        for month in os.listdir(os.path.join("text_jsonl", year)):
+            for day in os.listdir(os.path.join("text_jsonl", year, month)):
+                get_sentiment(int(year), int(month), int(day[:2]))
     # get_sentiment(2023, 4, 4)
+
 
 get_sentiment_all_dates()
