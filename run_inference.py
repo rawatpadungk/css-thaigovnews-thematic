7adce5c10e119576a37c52b3f53e968b64de3834
autosunpakorn <autorobot41@gmail.com>
2024-05-18 15:48:12 +0900

    add try except

--- run_inference.py
+++ run_inference.py
@@ -66,8 +66,10 @@ def get_sentiment_all_dates():
     for year in os.listdir("text_jsonl"):
         for month in os.listdir(os.path.join("text_jsonl", year)):
             for day in os.listdir(os.path.join("text_jsonl", year, month)):
-                get_sentiment(int(year), int(month), int(day[:2]))
-    # get_sentiment(2023, 4, 4)
+                try:
+                    get_sentiment(int(year), int(month), int(day[:2]))
+                except:
+                    print(f"Error: {year}-{month}-{day[:2]}")
 
 
 get_sentiment_all_dates()
