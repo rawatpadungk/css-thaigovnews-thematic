6256d76bcbf14196b0bde5e4e8378c9318d83c08
autosunpakorn <autorobot41@gmail.com>
2024-05-28 15:07:17 +0900

    add sentiment score by month

--- analysis/sentiment_score_distribution.py
+++ analysis/sentiment_score_distribution.py
@@ -2,27 +2,28 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
 import os
+import numpy as np
 import json
 
 
-def get_avg_sentiment_score(year: int, month: int, day: str):
-    avg_score_one_day = []
-    open_path = os.path.join("sentiment_jsonl", str(year), str(month).zfill(2), str(day))
-    with open(open_path, "r", encoding="utf8") as infile:
-        for line in infile:
-            data = json.loads(line)
-            avg_score_one_day.append(data["avg_score"])
-    return avg_score_one_day
+def get_sentiment_score_by_month(year: int, month: int):
+    avg_score_one_month = []
+    for day in os.listdir(os.path.join("sentiment_jsonl", str(year), str(month).zfill(2))):
+        open_path = os.path.join("sentiment_jsonl", str(year), str(month).zfill(2), str(day))
+        with open(open_path, "r", encoding="utf8") as infile:
+            for line in infile:
+                data = json.loads(line)
+                avg_score_one_month.append(data["avg_score"])
+    return avg_score_one_month
 
 
 def get_sentiment_score_distribution():
     all_avg_scores = []
-    save_path = "analysis/sentiment_score_distribution.png"
     for year in os.listdir("sentiment_jsonl"):
         for month in os.listdir(os.path.join("sentiment_jsonl", year)):
-            for day in os.listdir(os.path.join("sentiment_jsonl", year, month)):
-                all_avg_scores.extend(get_avg_sentiment_score(int(year), int(month), day))
+            all_avg_scores.extend(get_sentiment_score_by_month(int(year), int(month)))
 
+    save_path = "analysis/sentiment_score_distribution.png"
     plt.figure(figsize=(10, 6))
     sns.displot(data=all_avg_scores)
     plt.title("Distribution of Scores")
@@ -32,5 +33,27 @@ def get_sentiment_score_distribution():
     plt.show()
 
 
+def plot_sentiment_score_by_month():
+    all_months = []
+    score_by_month = []
+
+    for year in os.listdir("sentiment_jsonl"):
+        for month in os.listdir(os.path.join("sentiment_jsonl", year)):
+            all_months.append(f"{year}-{month}")
+            score_by_month.append(np.mean(get_sentiment_score_by_month(int(year), int(month))))
+
+    df = pd.DataFrame({"month": all_months, "avg_score": score_by_month})
+    save_path = "analysis/sentiment_score_by_month.png"
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=df, x="month", y="avg_score")
+    plt.title("Average Sentiment Score by Month")
+    plt.xlabel("Month")
+    plt.ylabel("Average Sentiment Score")
+    plt.xticks(range(len(all_months)), all_months, rotation=45)
+    plt.savefig(save_path)
+    plt.show()
+
+
 if __name__ == "__main__":
     get_sentiment_score_distribution()
+    plot_sentiment_score_by_month()
