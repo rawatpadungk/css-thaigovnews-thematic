bd81e4fb69090c62479e8d716d0ddb511a98b87f
autosunpakorn <autorobot41@gmail.com>
2024-06-04 17:32:28 +0900

    code cleaned

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ To reproduce our project, please run all 3 files in the 'core' directory in this
 | Dataset | Description |
 | ----- |  ---- |
 | thaigov-v2-corpus (2022/11-2024/05)  | Download the dataset [here](https://drive.google.com/file/d/1fIqR5OBE7UfNQzR9o3dyNUY7ZLw2y1N3/view?usp=sharing), unzip and move it to `data`.|
+This contains a total of 24021 articles.
 
 ### Preprocessed Data
 Download the preprocessed data [here](https://drive.google.com/drive/folders/1eGH0AlaW8c6JTQFiakkAEpV8V391dmgC?usp=sharing)
---
+++ core/analyze_result.py
@@ -0,0 +1,159 @@
+import json
+import os
+import random
+from collections import defaultdict
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from conversion import THAI_TO_ENG_TOPIC, FULLNAME_TO_ABBREVIAION
+
+FIGSIZE_RECT = (12.5, 6.5)
+FIGSIZE_SQR = (10, 10)
+
+random.seed(42)
+
+
+class Analysis:
+    def __init__(self):
+        self.all_months = self.get_all_months()
+        self.all_topics = self.get_all_topics()
+        self.monthly_score_by_topic = self.get_monthly_score_by_topic()
+        self.all_scores = self.get_all_scores()
+        os.makedirs("visual_analysis", exist_ok=True)
+
+    def plot_all_visualizations(self):
+        self.barplot_freq_by_topic()
+        self.histplot_score_distribution()
+        self.boxplot_score_by_topic()
+        for specific_type in ["frequent", "positive", "negative"]:
+            self.lineplot_monthly_score_by_specific_topics(specific_type=specific_type)
+
+    def get_all_months(self):
+        all_months = []
+        for year in os.listdir("sentiment_jsonl"):
+            for month in os.listdir(os.path.join("sentiment_jsonl", year)):
+                all_months.append(f"{year}-{month}")
+        return all_months
+
+    def get_all_topics(self):
+        return FULLNAME_TO_ABBREVIAION.keys()
+
+    def get_monthly_score_by_topic(self):
+        monthly_score_by_topic = defaultdict(lambda: {topic: [] for topic in self.all_topics})
+        for year in os.listdir("sentiment_jsonl"):
+            for month in os.listdir(os.path.join("sentiment_jsonl", year)):
+                for day in os.listdir(os.path.join("sentiment_jsonl", year, month)):
+                    open_path = os.path.join("sentiment_jsonl", year, month, day)
+                    with open(open_path, "r", encoding="utf8") as infile:
+                        for line in infile:
+                            data = json.loads(line)
+                            year_month = f"{year}-{month}"
+                            converted_topic = THAI_TO_ENG_TOPIC.get(data["topic_model"], "Others")
+                            monthly_score_by_topic[year_month][converted_topic].append(data["avg_score"])
+        return pd.DataFrame(monthly_score_by_topic).T
+
+    def get_avg_monthly_score_by_topic(self):
+        return self.monthly_score_by_topic.map(np.mean)
+
+    def get_monthly_freq_by_topic(self):
+        return self.monthly_score_by_topic.map(len)
+
+    def get_all_scores(self):
+        all_scores = sum(self.monthly_score_by_topic.values.flatten(), [])
+        self.mean_score = np.mean(all_scores)
+        self.median_score = np.median(all_scores)
+        return all_scores
+
+    def barplot_freq_by_topic(self):
+        df = self.get_monthly_freq_by_topic().sum(axis=0).sort_values()
+        self.frequent_topics = df.index[-4:]
+        plt.figure(figsize=FIGSIZE_SQR)
+        sns.barplot(x=[FULLNAME_TO_ABBREVIAION.get(topic) for topic in df.index], y=df.values, color="#029386")
+        plt.rcParams.update({"font.size": 26})
+        plt.title("Frequency by Topic")
+        plt.xlabel("")
+        plt.ylabel("Frequency", fontsize=22)
+        plt.xticks(rotation=90, fontsize=18)
+        plt.yticks(fontsize=18)
+        plt.tight_layout()
+        plt.savefig("visual_analysis/frequency_by_topic.png")
+
+    def histplot_score_distribution(self):
+        plt.figure(figsize=FIGSIZE_SQR)
+        sns.histplot(self.all_scores, bins=20, color="orange")
+        plt.axvline(self.median_score, color="red", linestyle="--", label="median score")
+        plt.legend()
+        plt.rcParams.update({"font.size": 26})
+        plt.title("Distribution of Scores")
+        plt.xlabel("Average Sentiment Score")
+        plt.ylabel("Frequency")
+        plt.xticks(fontsize=18)
+        plt.yticks(fontsize=18)
+        plt.tight_layout()
+        plt.savefig("visual_analysis/sentiment_score_distribution.png")
+
+    def boxplot_score_by_topic(self):
+        score_by_topic = self.monthly_score_by_topic.apply(lambda col: sum(col, []), axis=0)
+        sort_idx = score_by_topic.apply(np.mean).sort_values().index
+        self.positive_topics = sort_idx[-4:]
+        self.negative_topics = sort_idx[:4]
+        df = score_by_topic[sort_idx]
+        plt.figure(figsize=FIGSIZE_RECT)
+        plt.rcParams.update({"font.size": 16})
+        plt.boxplot(
+            df,
+            labels=[FULLNAME_TO_ABBREVIAION.get(topic) for topic in df.index],
+            patch_artist=True,
+            showmeans=True,
+            boxprops=dict(facecolor="lightblue"),
+            medianprops=dict(color="black", linewidth=2.5),
+            meanprops=dict(markerfacecolor="#E50000", markeredgecolor="#E50000", markersize=10),
+        )
+        plt.title("Sentiment Score by Topic")
+        plt.xlabel("")
+        plt.ylabel("Sentiment Score")
+        plt.xticks(rotation=75, fontsize=15)
+        plt.tight_layout()
+        plt.savefig("visual_analysis/score_by_topic.png")
+
+    def lineplot_monthly_score_by_specific_topics(self, specific_type=None):
+        if specific_type == "frequent":
+            specific_topics = self.frequent_topics
+        elif specific_type == "positive":
+            specific_topics = self.positive_topics
+        elif specific_type == "negative":
+            specific_topics = self.negative_topics
+        else:
+            specific_topics = self.all_topics
+        avg_monthly_score_by_topic = self.get_avg_monthly_score_by_topic()
+        avg_monthly_score = self.monthly_score_by_topic.apply(lambda row: sum(row, []), axis=1).apply(np.mean)
+        plt.figure(figsize=FIGSIZE_RECT)
+        plt.rcParams.update({"font.size": 16})
+        plt.axhline(self.mean_score, color="grey", linestyle="--", label="mean score")
+        sns.lineplot(
+            x=avg_monthly_score.index,
+            y=avg_monthly_score.values,
+            label="avg monthly score",
+            linewidth=2.5,
+            color="black",
+            linestyle="--",
+        )
+        df = avg_monthly_score_by_topic.interpolate().loc[:, specific_topics]
+        colors = ["red", "#FFA500", "green", "#0077B6"]
+        for i, topic in enumerate(df.columns):
+            sns.lineplot(x=df.index, y=df[topic], color=colors[i], label=topic)
+        plt.legend(fontsize=14, ncol=2, loc="lower left")
+        plt.title("Average Monthly Sentiment Score of Frequent Topic")
+        plt.xlabel("")
+        plt.ylabel("Average Sentiment Score", fontsize=15)
+        plt.xticks(range(len(self.all_months)), self.all_months, rotation=45, fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.tight_layout()
+        plt.savefig(f"visual_analysis/monthly_score_by_{specific_type}_topic.png")
+
+
+if __name__ == "__main__":
+    Analysis().plot_all_visualizations()
